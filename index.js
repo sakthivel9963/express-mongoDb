b0b062451a4b345c53445bbbaf9b6a63e31132bb
sakthivel9963 <sakthivel9963@gmail.com>
2019-09-18 14:50:50 +0530

    split the project into different folder

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-app.get('/', (req, res) => {
-  res.send(`hello`);
-});
+const app = require('./src/app.js');
 
 app.listen(process.env.PORT, () => {
   console.log(
