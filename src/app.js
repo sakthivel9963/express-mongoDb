11a2e3916f7ea1814e365e9ce73d3c161f91d339
sakthivel9963 <sakthivel9963@gmail.com>
2019-09-18 19:57:23 +0530

    :tada: mongoose added connection established

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
+require('../config/db');
+
 const router = require('./routes');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
